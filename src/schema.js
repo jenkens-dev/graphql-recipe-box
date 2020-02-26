a95c259cddd386bea0ce6614af0998b718283535
Taljjaa <jenken131@gmail.com>
2020-02-26 10:58:46 -0800

    first commit, created apollo server and basic schema

---
+++ src/schema.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+   type User {
+      id: ID!
+      savedRecipes: [Recipe]!
+   }
+
+   type Recipe {
+      id: ID!
+      name: String!
+      instructions: String!
+      picture: String!
+      categories: [Tag]!
+   }
+
+   type Tag {
+      id: ID!
+      name: String!
+   }
+`;
+
+module.exports = typeDefs;

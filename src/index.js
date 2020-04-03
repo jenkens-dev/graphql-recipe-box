6ed097bf94a3f1d9cd29e3fe308621175b1d10db
Taljjaa <jenken131@gmail.com>
2020-04-03 14:24:21 -0700

    added db to index and resolvers to fetch data from db, also added graphql playground middleware to test queries in the playground

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const startDatabase = require('./database');
+const expressPlayground = require('graphql-playground-middleware-express')
+   .default;
+
+// Create a context for holding contextual data
+const context = async () => {
+   const db = await startDatabase();
+
+   return { db };
+};
 
 //Provide resolver fucntions for your schema fields
 const resolvers = {
-   hello: () => 'Hello world!',
+   recipes: async (_, context) => {
+      const { db } = await context();
+      return db
+         .collection('recipes')
+         .find()
+         .toArray();
+   },
+   recipe: async ({ id }, context) => {
+      const { db } = await context();
+      return db.collection('recipes').findOne({ id });
+   },
 };
 
 const app = express();
-
 app.use(
    '/graphql',
    graphqlHTTP({
       schema,
       rootValue: resolvers,
+      context,
    }),
 );
 
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 app.listen(4000);
 
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
